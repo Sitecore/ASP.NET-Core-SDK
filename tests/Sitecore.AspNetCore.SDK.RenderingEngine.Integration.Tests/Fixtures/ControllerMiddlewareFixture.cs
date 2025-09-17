5f6fecb7dbd44a501d310a676b04ad6b2245506b
krishanthaudayakumara <udayakumarakrishantha@gmail.com>
2025-09-17 13:22:33 +0530

    Migrate ControllerMiddlewareFixture

--- tests/Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests/Fixtures/ControllerMiddlewareFixture.cs
+++ tests/Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests/Fixtures/ControllerMiddlewareFixture.cs
@@ -1,45 +1,22 @@
 ﻿using System.Net;
 using AwesomeAssertions;
-using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Sitecore.AspNetCore.SDK.AutoFixture.Mocks;
 using Sitecore.AspNetCore.SDK.LayoutService.Client.Extensions;
+using Sitecore.AspNetCore.SDK.RenderingEngine.Extensions;
 using Xunit;
 
 namespace Sitecore.AspNetCore.SDK.RenderingEngine.Integration.Tests.Fixtures;
 
-public class ControllerMiddlewareFixture : IDisposable
+public class ControllerMiddlewareFixture(TestWebApplicationFactory<TestWebApplicationProgram> factory) : IClassFixture<TestWebApplicationFactory<TestWebApplicationProgram>>, IDisposable
 {
     private const string MiddlewareController = "ControllerMiddleware";
 
     private const string GlobalMiddlewareController = "GlobalMiddleware";
 
-    private readonly TestServer _server;
-
-    private readonly MockHttpMessageHandler _mockClientHandler;
+    private readonly MockHttpMessageHandler _mockClientHandler = new MockHttpMessageHandler();
 
     private readonly Uri _layoutServiceUri = new("http://layout.service");
-
-    public ControllerMiddlewareFixture()
-    {
-        TestServerBuilder testHostBuilder = new();
-        _mockClientHandler = new MockHttpMessageHandler();
-        testHostBuilder
-            .ConfigureServices(builder =>
-            {
-                builder
-                    .AddSitecoreLayoutService()
-                    .AddHttpHandler(
-                        "mock",
-                        _ => new HttpClient(_mockClientHandler)
-                        {
-                            BaseAddress = _layoutServiceUri
-                        })
-                    .AsDefaultHandler();
-            })
-            .Configure(_ => { });
-
-        _server = testHostBuilder.BuildServer(new Uri("http://localhost"));
-    }
 
     [Fact]
     public async Task HttpClient_IsInvoked()
@@ -49,7 +26,7 @@ public class ControllerMiddlewareFixture : IDisposable
             StatusCode = HttpStatusCode.OK
         });
 
-        HttpClient client = _server.CreateClient();
+        HttpClient client = BuildControllerMiddlewareWebApplicationFactory().CreateClient();
         await client.GetAsync(MiddlewareController);
 
         _mockClientHandler.WasInvoked.Should().BeTrue();
@@ -63,7 +40,7 @@ public class ControllerMiddlewareFixture : IDisposable
             StatusCode = HttpStatusCode.OK
         });
 
-        HttpClient client = _server.CreateClient();
+        HttpClient client = BuildControllerMiddlewareWebApplicationFactory().CreateClient();
         await client.GetAsync(GlobalMiddlewareController);
 
         _mockClientHandler.WasInvoked.Should().BeFalse();
@@ -77,7 +54,7 @@ public class ControllerMiddlewareFixture : IDisposable
             StatusCode = HttpStatusCode.OK
         });
 
-        HttpClient client = _server.CreateClient();
+        HttpClient client = BuildControllerMiddlewareWebApplicationFactory().CreateClient();
         string response = await client.GetStringAsync(GlobalMiddlewareController);
 
         response.Should().Be("\"success\"");
@@ -91,7 +68,7 @@ public class ControllerMiddlewareFixture : IDisposable
             StatusCode = HttpStatusCode.OK
         });
 
-        HttpClient client = _server.CreateClient();
+        HttpClient client = BuildControllerMiddlewareWebApplicationFactory().CreateClient();
         await client.GetAsync(MiddlewareController);
 
         _mockClientHandler.Requests.Single().RequestUri!.AbsoluteUri.Should()
@@ -100,8 +77,35 @@ public class ControllerMiddlewareFixture : IDisposable
 
     public void Dispose()
     {
-        _server.Dispose();
         _mockClientHandler.Dispose();
         GC.SuppressFinalize(this);
+    }
+
+    private WebApplicationFactory<TestWebApplicationProgram> BuildControllerMiddlewareWebApplicationFactory()
+    {
+        return factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services
+                    .AddRouting()
+                    .AddSitecoreLayoutService()
+                    .AddHttpHandler(
+                        "mock",
+                        _ => new HttpClient(_mockClientHandler)
+                        {
+                            BaseAddress = _layoutServiceUri
+                        })
+                    .AsDefaultHandler();
+
+                services.AddSitecoreRenderingEngine();
+            });
+
+            builder.Configure(app =>
+            {
+                app.UseRouting();
+                app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            });
+        });
     }
 }
